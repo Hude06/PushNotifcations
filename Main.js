2da879337727619acf455f71fd74b2e89e8ac447
judeallanhill <judeallanhill@icloud.com>
2024-01-03 09:43:18 -0500

    push

--- Main.js
+++ Main.js
@@ -1,19 +1,8 @@
-console.log("Running")
-document.getElementById("Notify").addEventListener("click", () => {
-  console.log("Clicked")
-  Notification.requestPermission().then((result) => {
-    if (result === "granted") {
-      document.getElementById("text").innerHTML = "Granted"
-    console.log("granred")
-      const text = `HEY! Your task is now overdue.`;
-      const notification = new Notification("To-do List", { body: text });
-      console.log(notification)
-      document.getElementById("notify").innerHTML = notification.body
-      notification.onclick = function() {
-        alert("Notification clicked!");
-      };
-    }
+navigator.serviceWorker.register('ServiceWorker.js')
+  .then(registration => {
+    console.log('Service Worker registered with scope:', registration.scope);
+  })
+  .catch(error => {
+    console.error('Service Worker registration failed:', error);
   });
 
-})
-  

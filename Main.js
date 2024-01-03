df6ae77d5853e3373345a667befacd45d3454073
judeallanhill <judeallanhill@icloud.com>
2024-01-03 08:13:06 -0500

    Ading New HTML

--- Main.js
+++ Main.js
@@ -3,6 +3,7 @@ document.getElementById("Notify").addEventListener("click", () => {
   console.log("Clicked")
   Notification.requestPermission().then((result) => {
     if (result === "granted") {
+      document.getElementById("text").innerHTML = "Granted"
     console.log("granred")
       const text = `HEY! Your task is now overdue.`;
       const notification = new Notification("To-do List", { body: text });
